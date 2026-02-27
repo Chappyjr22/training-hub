d5074f77b89dece61c61378ee48c08aec7043acc
Chappyjr22 <Chappyjr22@users.noreply.github.com>
2026-02-27 13:57:56 +0000

    deploy: bf68e972fc5edc0112bca015261b13349d249a7c

--- _next/static/mKKcbhkgcL4kc5LxS4OSq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2e27c735b74e98a8.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],"/training/[id]":["static/chunks/pages/training/[id]-4592beb0bbe1936b.js"],sortedPages:["/","/_app","/_error","/training/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
